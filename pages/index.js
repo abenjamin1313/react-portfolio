9d682ab557f2e215a859abc8cd2c4fe0d55825f5
Adam Victor <abenjamin1313.com>
2022-10-02 12:26:10 -0400

    Basic navbar.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,9 +10,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 classaName="text-xl font-bold">Adam M. Victor</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 ml-8 rounded-md"
+                  alt="Resume"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div>
+          
+          </div>
         </section>
       </main>
     </div>
